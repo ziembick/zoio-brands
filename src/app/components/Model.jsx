2c11b8213ca4631278ae0d76688173d2019551fa
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-12 00:46:05 -0300

    para dev

--- src/app/components/Model.jsx
+++ src/app/components/Model.jsx
@@ -20,15 +20,28 @@ export default function Model() {
     }
   });
 
-  const materialProps = useControls({
-    thickness: { value: 1.25, min: 0, max: 3, step: 0.05 },
-    roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-    transmission: { value: 0.99, min: 0, max: 1, step: 0.1 },
-    ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-    chromaticAberration: { value: 0.02, min: 0, max: 1 },
-    backside: { value: true },
-  });
-
+  let materialProps = {};
+  if (process.env.NODE_ENV === "development") {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    materialProps = useControls({
+      thickness: { value: 1.25, min: 0, max: 3, step: 0.05 },
+      roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+      transmission: { value: 0.99, min: 0, max: 1, step: 0.1 },
+      ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+      chromaticAberration: { value: 0.02, min: 0, max: 1 },
+      backside: { value: true },
+    });
+  } else {
+    // Valores padrão para produção
+    materialProps = {
+      thickness: 1.25,
+      roughness: 0,
+      transmission: 0.99,
+      ior: 1.2,
+      chromaticAberration: 0.02,
+      backside: true,
+    };
+  }
   return (
     <group>
       <Text position={[0, 0, -0.5]} fontSize={0.75} color="white">
@@ -37,7 +50,6 @@ export default function Model() {
       
       <mesh ref={mesh} {...nodes.Curve} position={[0, 0, 0]} color="green">
         <MeshTransmissionMaterial {...materialProps} />
-      <OrbitControls />
       </mesh>
     </group>
   );
