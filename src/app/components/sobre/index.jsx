71f258e759866d91d40daeb6a5aa9bcacb9839f7
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-27 21:59:31 -0300

    animação sobre

--- src/app/components/sobre/index.jsx
+++ src/app/components/sobre/index.jsx
@@ -5,7 +5,6 @@ import styles from "./sobre.module.sass";
 import { gsap } from "gsap";
 import { CustomEase } from "gsap/dist/CustomEase";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { motion } from "framer-motion";
 
 gsap.registerPlugin(CustomEase, ScrollTrigger);
 CustomEase.create("cubic-text", "0.25, 1, 0.5, 1");
@@ -13,13 +12,12 @@ CustomEase.create("cubic-text", "0.25, 1, 0.5, 1");
 export default function Sobre() {
   const triggerRef = useRef(null);
   const h1Ref = useRef(null);
-  // const h6Ref = useRef(null);
+  const h6Ref = useRef(null);
 
   useEffect(() => {
     const h1Spans = h1Ref.current.querySelectorAll("span");
-    // const h6Spans = h6Ref.current.querySelectorAll("span");
 
-    const tl = gsap.timeline({
+    const h1Timeline = gsap.timeline({
       scrollTrigger: {
         trigger: triggerRef.current,
         start: "top center",
@@ -28,11 +26,10 @@ export default function Sobre() {
       },
     });
 
-    // [h1Spans, h6Spans].forEach((spans, index) => {
-    [h1Spans].forEach((spans, index) => {
-      const delay = index * 1.2;
-      tl.to(
-        spans,
+    h1Spans.forEach((span, index) => {
+      const delay = index * 0.1; // Ajuste o delay conforme necessário
+      h1Timeline.to(
+        span,
         {
           y: 0,
           opacity: 1,
@@ -45,8 +42,29 @@ export default function Sobre() {
       );
     });
 
+    const h6Timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerRef.current,
+        start: "top center",
+        end: "bottom 80%",
+        scrub: 1,
+      },
+    });
+
+    h6Timeline.to(
+      h6Ref.current.querySelector("span"),
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1.5,
+        ease: "cubic-text",
+      },
+      0
+    );
+
     return () => {
-      tl.kill();
+      h1Timeline.kill();
+      h6Timeline.kill();
     };
   }, []);
 
@@ -65,27 +83,16 @@ export default function Sobre() {
               </span>
             ))}
         </h1>
-        {/* <h6 ref={h6Ref} className={`${styles.textos} ${styles.aga6} pb-5`}> */}
-
-        <motion.h4
-          className={`${styles.textos} ${styles.aga6} ${styles.container}pb-5`}
-          initial={{ opacity: 0, y: -100 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -100 }}
-          transition={{
-            type: "spring",
-            stiffness: 100,
-            damping: 30,
-            delay: 0.1,
-          }}
-        >
-          Desenvolvemos identidades visuais que não apenas representam a
-          essência de cada marca, mas que também inspirem novas percepções e
-          conexões. Através de estratégias e um olhar atento aos detalhes,
-          transformamos visões em realidades visuais impactantes. Queremos que
-          cada marca que tocamos seja vista, sentida e transformada,
-          proporcionando experiências únicas e memoráveis.
-        </motion.h4>
+        <h6 ref={h6Ref} className={`${styles.textos} ${styles.aga6} pb-5`}>
+          <span className="inline-block translate-y-full opacity-0">
+            Desenvolvemos identidades visuais que não apenas representam a
+            essência de cada marca, mas que também inspirem novas percepções e
+            conexões. Através de estratégias e um olhar atento aos detalhes,
+            transformamos visões em realidades visuais impactantes. Queremos que
+            cada marca que tocamos seja vista, sentida e transformada,
+            proporcionando experiências únicas e memoráveis.
+          </span>
+        </h6>
       </div>
     </div>
   );
