8baf82321e6e9b645071201d588f4d9174ac83be
Paulo Ziembick <pauloziembick@gmail.com>
2024-08-15 23:15:01 -0300

    horizontal do video

--- src/app/components/zoio/index.jsx
+++ src/app/components/zoio/index.jsx
@@ -29,7 +29,7 @@ export default function ZoioVideo() {
 
   return (
     <div className="flex flex-col items-center justify-center w-full h-full">
-      <div className={`${styles.videoContainer} w-full h-full`} ref={videoRef}>
+      <div className={`${styles.videoContainer}`} ref={videoRef}>
         <video
           src="/videos/zoiovideo.mp4"
           controls
